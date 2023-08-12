ba1bc0af199ca38097dd0bc4808c969679e8486f
Max Rosenblattl <max.rosenblattl@tum.de>
2023-08-13 00:19:54 +0200

    adapt for production

--- decentralized-news/src/services/api.tsx
+++ decentralized-news/src/services/api.tsx
@@ -1,7 +1,12 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+const isProduction = true;
+const url = isProduction
+    ? "https://decent-news-api.onrender.com:10000/api"
+    : "http://localhost:3500/api/";
+
 export const api = createApi({
     reducerPath: "api",
-    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3500/api/" }),
+    baseQuery: fetchBaseQuery({ baseUrl: url }),
     endpoints: () => ({}),
 });
