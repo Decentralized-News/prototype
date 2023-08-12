48d68262d069e5eee8b9e384de1287c78b479d68
Max Rosenblattl <max.rosenblattl@tum.de>
2023-08-12 13:35:17 +0200

    Implement create article page

--- decentralized-news/src/App.tsx
+++ decentralized-news/src/App.tsx
@@ -1,28 +1,31 @@
 import {
-    createBrowserRouter,
-    Navigate,
-    RouterProvider,
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
 } from "react-router-dom";
 import "./App.css";
 import Home from "./components/home/Home";
-import {ConfigProvider} from "antd";
+import { ConfigProvider } from "antd";
+import CreateArticle from "./components/create/CreateArticle";
 
 function App() {
-    const router = createBrowserRouter([
-        {path: "/", element: <Home/>},
-        {path: "*", element: <Navigate to="/"/>},
-    ]);
+  const router = createBrowserRouter([
+    { path: "/", element: <Home /> },
+    { path: "/create", element: <CreateArticle /> },
+    { path: "*", element: <Navigate to="/" /> },
+  ]);
 
-    return (
-        <ConfigProvider
-            theme={{
-                token: {
-                    colorPrimary: "#7465BE",
-                },
-            }}
-        >
-            <RouterProvider router={router}></RouterProvider>
-        </ConfigProvider>);
+  return (
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: "#7465BE",
+        },
+      }}
+    >
+      <RouterProvider router={router}></RouterProvider>
+    </ConfigProvider>
+  );
 }
 
 export default App;
